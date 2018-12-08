8e8efd44edbb6b219ba756bf70c4f533024f2433
Copple <pcopplestone@gmail.com>
2018-12-08 15:39:16 +0800

    Cleaning up Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,27 @@ import { Link } from "react-router-dom";
 
 export class Footer extends React.Component {
   render() {
-    
     return (
       <footer className="footer">
-          <div className="container">
-            <div className="row">
-              <div className="columns small-6">Footer</div>
-              <div className="columns small-6">
-              Links
-              </div>
-            </div>
+        <div className="row">
+          <div className="columns small-6">TITLE</div>
+          <div className="columns small-2">
+            <ul>
+              <li>Terms</li>
+              <li>Privacy</li>
+            </ul>
           </div>
+          <div className="columns small-2">
+            <ul>
+              <li>
+                <Link to={`/catalogue`} className="">
+                  Catalogue
+                </Link>
+              </li>
+              <li>Search</li>
+            </ul>
+          </div>
+        </div>
       </footer>
     );
   }
