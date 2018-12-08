5f8a404f6fb689012566a1f26489eb20babbebe0
Copple <pcopplestone@gmail.com>
2018-12-08 15:58:38 +0800

    Hero width on HomePage

--- src/components/pure/Hero.js
+++ src/components/pure/Hero.js
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 export class Hero extends React.PureComponent {
   render() {
     return (
-      <div id="Hero" className="row">
+      <div id="Hero">
+        <div className="row">
           <div className="column">
             <h1>Sell your misleading thumbnails</h1>
             <Link to={`/catalogue`} className="button" type="a">
-                  Commit Bid
-                </Link>
+              Commit Bid
+            </Link>
           </div>
           <div className="column">Images here </div>
+        </div>
       </div>
     );
   }
