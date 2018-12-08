c16f2325c35e180761522396e80d02c8a05821e2
Copple <pcopplestone@gmail.com>
2018-12-08 15:36:35 +0800

    Fixing keys on CateloguePage

--- src/pages/Catalogue.js
+++ src/pages/Catalogue.js
@@ -15,10 +15,9 @@ export class CataloguePage extends React.Component {
     const { products } = this.state;
     
     let ProductList = products.map(product => (
-      <div className="columns small-6  medium-4 ">
+      <div className="columns small-6 medium-4 " key={`product.${product.id}`}>
         <ProductCard
           id={product.id}
-          key={product.id}
           url={product.thumb}
           title={product.title}
         />
