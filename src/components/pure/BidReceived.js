9b4500935ac22306ea23c73712e79ec01b677c7d
mildtomato <jon.summers.muir@googlemail.com>
2018-12-08 20:38:15 +0800

    added some icon anims

--- src/components/pure/BidReceived.js
+++ src/components/pure/BidReceived.js
@@ -23,8 +23,8 @@ export class BidReceived extends React.PureComponent {
           <div className="column align-self-middle"><h3 className="text-primary"><i className="fab fa-ethereum" style={{marginRight: 16}}></i>{ether} ETH</h3></div>
           <div className="column large-4 text-right">
             <div className="button-group large">
-              <button className="button primary" onClick={onAccept}><i className="far fa-check-circle"></i> Accept Bid</button>
-              <button className="button alert" onClick={onDecline}><i className="far fa-times-circle"></i></button>
+              <button className="button primary hvr-icon-grow" onClick={onAccept}><i className="far fa-check-circle hvr-icon"></i> Accept Bid</button>
+              <button className="button alert hvr-icon-grow" onClick={onDecline}><i className="far fa-times-circle hvr-icon"></i></button>
             </div>
           </div>
         </div>
